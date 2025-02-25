01755959070eb2b554249a2e38be76c622a5c9b3
FerranBCalpe <ferran.calpe@gmail.com>
2025-02-25 19:53:36 +0100

    Creadas las mockup de players, y añadido los servicios,

--- src/app/player-card/player-card.component.ts
+++ src/app/player-card/player-card.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../services/mockup-players';
+import { PlayerService } from '../services/playerService';
 
 @Component({
   selector: 'app-player-card',
-  imports: [],
   templateUrl: './player-card.component.html',
   styleUrl: './player-card.component.css'
 })
 export class PlayerCardComponent {
 
+  players: Player[] = [];
+
+  constructor(private playerService: PlayerService) { }
+
+  ngOnInit(): void {
+    this.getPlayers();
+  }
+
+  getPlayers(): void {
+    this.players= this.playerService.getPlayers().slice(1, 6);;
+    }
 }
+
+
