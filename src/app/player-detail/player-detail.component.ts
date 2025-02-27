a8656e2c9b5d6af5decca5c58c01f0e7b0b3963f
fcalper@uoc.edu <ferran.calpe@gmail.com>
2025-02-27 19:59:34 +0100

    probando de pasar el player por [state]

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../services/playerService';
 import {Player} from '../services/player';
-import { ActivatedRoute } from '@angular/router';
+import {Router, ActivatedRoute } from '@angular/router';
 import {Players} from '../services/mockup-players';
+import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-player-detail',  
+  standalone: true,
+  imports:[CommonModule,RouterModule],
   templateUrl: './player-detail.component.html',
   styleUrl: './player-detail.component.css',
   
 })
-export class PlayerDetailComponent implements OnInit {
-  player: Player | undefined;
+export class PlayerDetailComponent {
+  player?: Player;
   
   
 
-  constructor(private route: ActivatedRoute, private playerService: PlayerService) { };
-  ngOnInit():void {
-    this.getPlayerDetails();
+  constructor(private router: Router) { 
+    if (!this.player) {
+      console.warn('No player data received');
+    }
+    const navigation = this.router.getCurrentNavigation();
+    this.player = navigation?.extras.state?.['player'];
   }
-  getPlayerDetails(): void {
-    const playerId = 1; // Asigna el ID del jugador que quieres mostrar (sin depender de la URL)
-    this.player = Players.find(player => player.id === playerId);
   }
-}
+  
+//   ngOnInit():void {
+//     this.getPlayerDetails();
+//   }
+//   getPlayerDetails(): void {
+//     const playerId = this.player.id; // Asigna el ID del jugador que quieres mostrar (sin depender de la URL)
+//     this.player = Players.find(player => player.id === playerId);
+//   }
+// }
 
