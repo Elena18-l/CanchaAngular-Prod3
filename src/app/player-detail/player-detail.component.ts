cf5d3de61f0e1b1d0e5e06f142082766f3e70f7b
Elena <elopezestr@uoc.edu>
2025-02-28 10:26:49 +0100

    funcionadenuevo

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -5,17 +5,16 @@ import { CommonModule } from '@angular/common'; // <-- IMPORTAR CommonModule
 import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../services/playerService';
 import {Player} from '../services/player';
-import {Router, ActivatedRoute } from '@angular/router';
+import {Router, ActivatedRoute, RouterModule } from '@angular/router';
 import {Players} from '../services/mockup-players';
 import { Location } from '@angular/common';  //esto es para ir para atrás
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-player-detail',  
   standalone: true,
-  imports:[CommonModule,RouterModule],
   templateUrl: './player-detail.component.html',
   styleUrl: './player-detail.component.css',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
 })
 export class PlayerDetailComponent {
   player?: Player;
