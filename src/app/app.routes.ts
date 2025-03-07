c4881866818215c7d923f65cb30047027310a019
Elena <elopezestr@uoc.edu>
2025-03-07 10:56:43 +0100

    se murió dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { PlayerDetailComponent } from './player-detail/player-detail.component';
 import { PlayersComponent } from './players/players.component';
-import { PlayerCardComponent } from './player-card/player-card.component';
+import { PlayerCardComponent } from './player-card/player-card.component';	
 import { PlayerMediaComponent } from './player-media/player-media.component';
 
 export const appRoutes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/players', pathMatch: 'full' },  
   { path: 'players', component: PlayersComponent },
   { path: 'player/:id', component: PlayerDetailComponent },
   { path: 'player/:id/media', component: PlayerMediaComponent },
