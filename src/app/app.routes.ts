d3150e6e2e6fee4f4a023072d0d24334096738e3
Elena <elopezestr@uoc.edu>
2025-03-11 12:58:55 +0100

    media mas o menos arreglao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PlayerDetailComponent } from './player-detail/player-detail.component';
 import { PlayersComponent } from './players/players.component';
+import { PlayerMediaComponent } from './player-media/player-media.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'players', pathMatch: 'full' }, // Redirigir correctamente
   { path: 'players', component: PlayersComponent }, // Lista de jugadores
   { path: 'player/:id', component: PlayerDetailComponent }, // Detalle de un jugador
+  {path: 'player/:id/media', component: PlayerMediaComponent},
 ];
