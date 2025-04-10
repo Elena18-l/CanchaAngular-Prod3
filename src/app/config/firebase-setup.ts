fb9a4e8694c4832dfb08abe28eec035c187a8dcc
Guillem <guillemr4@gmail.com>
2025-04-10 14:25:54 +0200

    html uwu - elena

---
+++ src/app/config/firebase-setup.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    production: false,
+      apiKey: "AIzaSyBbK1Us_lbJytCFTrLzFjy2CN1gYA3sla0",
+      authDomain: "cancha-angular.firebaseapp.com",
+      databaseURL: "https://cancha-angular-default-rtdb.europe-west1.firebasedatabase.app/",
+      projectId: "cancha-angular",
+      storageBucket: "cancha-angular.appspot.com",
+      messagingSenderId: "731488175013",
+      appId: "1:731488175013:web:6c62e9f77407a855119e4c",
+      measurementId: "G-2SC9QSB2WG"
+    };
