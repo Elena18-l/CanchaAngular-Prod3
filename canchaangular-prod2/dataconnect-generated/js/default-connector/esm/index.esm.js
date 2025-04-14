fb6675f847e921b9d527d6dcba25702f783c9048
Elena <elopezestr@uoc.edu>
2025-04-14 13:03:20 +0200

    setteando expo

--- canchaangular-prod2/dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'canchaangular-prod2',
-  location: 'us-central1'
-};
-
--- canchaangular-prod2/dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,8 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'canchaangular-prod2',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
