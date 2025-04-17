65fe2a70449223272890ca0d1e01724ae9eefc11
Elena <elopezestr@uoc.edu>
2025-04-17 12:22:14 +0200

    safe commit

--- cancha-angular/App.tsx
+++ cancha-angular/App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TeamScreen from './screens/Team';
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {Layout} from './components/Layout';
 export default function App() {
   return (
+    <SafeAreaProvider>
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <TeamScreen></TeamScreen>
+      
+      <Layout/>
     </View>
+    </SafeAreaProvider>
   );
 }
 
