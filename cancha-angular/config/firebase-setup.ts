f5cec8a7e2c3a82e7db9a8b623b5212de655a5d6
Elena <elopezestr@uoc.edu>
2025-04-17 10:33:19 +0200

    firebaseeeeee UWU

---
+++ cancha-angular/config/firebase-setup.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+export const firebaseConfig = {
+  production: false,
+    apiKey: "AIzaSyBbK1Us_lbJytCFTrLzFjy2CN1gYA3sla0",
+    authDomain: "cancha-angular.firebaseapp.com",
+    databaseURL: "https://cancha-angular-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cancha-angular",
+    storageBucket: "cancha-angular.appspot.com",
+    messagingSenderId: "731488175013",
+    appId: "1:731488175013:web:6c62e9f77407a855119e4c",
+    measurementId: "G-2SC9QSB2WG"
+  };
+  const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
---
+++ screens/Player_detail.tsx
@@ -0,0 +1,17 @@
+/*import PentagonChart from './PentagonChart';
+
+const stats = {
+  fisico: 8,
+  tecnica: 7,
+  fuerzaMental: 9,
+  resistencia: 6,
+  habilidadEspecial: 7,
+};
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#111' }}>
+      <PentagonChart skills={stats} />
+    </View>
+  );
+}*/
